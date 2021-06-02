5c530cfcb338da4d70b19c3501aeadd0680f69dd
Eric Ostrowski <ericostrowski3@gmail.com>
2021-06-02 17:26:31 -0500

    edited javascript

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
+const btn = document.getElementById('btn');
 
 
-function turnBackgroundRed(){
-    document.body.style.backgroundColor = 'red'
-}
 
-document.body.button.onclick = turnBackgroundRed;
+btn.addEventListener('click', function(){
+    let hex = '#';
+    for(i = 0; i < 6; i++) {
+        let x = Math.floor(Math.random()*16);
+        switch (x){
+            case 10:
+                x = 'A';
+                break;
+            case 11:
+                x = 'B';
+                break;
+            case 12:
+                x = 'C';
+                break;
+            case 13:
+                x = 'D';
+                break;
+            case 14:
+                x = 'E';
+                break;
+            case 15:
+                x = 'F'
+                break;
+            default:
+                x = x;
+                break;
+        }
+        hex += x;
+        console.log(x);
+    }
+    console.log(hex);
+    document.body.style.backgroundColor = hex;
+    
+})
